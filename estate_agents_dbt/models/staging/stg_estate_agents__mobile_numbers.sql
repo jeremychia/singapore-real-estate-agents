7925e07cb1c37bc68359c7484973791de6ba15b5
jeremychia <jeremy.chia@vinted.com>
2024-09-06 16:01:48 +0200

    feat: dedup mobile numbers

--- estate_agents_dbt/models/staging/stg_estate_agents__mobile_numbers.sql
+++ estate_agents_dbt/models/staging/stg_estate_agents__mobile_numbers.sql
@@ -7,6 +7,16 @@ with
             registration_number as agent_registration_number,
             _accessed_at_utc as mobile_number_last_updated_at
         from source
+    ),
+    deduplicate as (
+        select *
+        from renamed
+        qualify
+            row_number() over (
+                partition by agent_registration_number
+                order by mobile_number_last_updated_at desc
+            )
+            = 1
     )
 select *
 from renamed
