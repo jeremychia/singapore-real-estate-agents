5ea1198b8cd1803212eee6deee56586bf9192dc3
jeremychia <jeremy.chia@vinted.com>
2024-09-06 22:45:53 +0200

    feat: deduplication for activities

--- estate_agents_dbt/models/staging/stg_estate_agents__private_sale.sql
+++ estate_agents_dbt/models/staging/stg_estate_agents__private_sale.sql
@@ -11,8 +11,14 @@ with
             property as property_type,
             client,
             rentaltype as transaction_type,
-            registrationnumber as agent_registration_number
+            registrationnumber as agent_registration_number,
+            _accessed_at_utc as last_updated_at_utc
         from source
+    ),
+    deduplicated as (
+        select *
+        from renamed
+        qualify row_number() over(partition by transaction_id order by last_updated_at_utc) = 1
     )
 select *
-from renamed
+from deduplicated
