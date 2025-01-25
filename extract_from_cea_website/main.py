75a2843ce16b9f671e110b667591a7227d15c313
jeremychia <jeremy.chia@vinted.com>
2025-01-25 10:34:40 +0800

    feat: add to main run

--- extract_from_cea_website/main.py
+++ extract_from_cea_website/main.py
@@ -3,6 +3,7 @@ import pandas_gbq
 
 import extract
 import load
+import load.deduplication as deduplication
 
 PROJECT_ID = "jeremy-chia"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "token/gcp_token.json"
@@ -19,13 +20,14 @@ directory_url = (
     "https://www.cea.gov.sg/aceas/api/internet/profile/v2/public-register/filter"
 )
 
-# agents_df = extract.iteratively_retrieve_data(directory_url, headers, directory_payload)
-# load.write_df_to_gbq(agents_df, "estate_agents", "agents", if_exists="append")
+agents_df = extract.iteratively_retrieve_data(directory_url, headers, directory_payload)
+load.write_df_to_gbq(agents_df, "estate_agents", "agents", if_exists="append")
 
 sql = """
 select distinct registrationNumber
 from `jeremy-chia.estate_agents.agents`
-where registrationNumber > 'R019369D'
+-- filter only when resuming run
+-- where registrationNumber > 'R019369D'
 order by registrationNumber asc
 """
 
@@ -34,3 +36,5 @@ agents_df = pandas_gbq.read_gbq(query_or_table=sql, project_id=PROJECT_ID)
 
 registration_numbers = list(agents_df["registrationNumber"])
 extract.retrieve_all_data_for_registration_numbers(registration_numbers, headers)
+
+deduplication.deduplicate_data(primary_key=["id"])
