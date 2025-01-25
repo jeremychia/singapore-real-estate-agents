5ecb91b62eb022555dbec1eaf8241d9b2697dce5
jeremychia <jeremy.chia@vinted.com>
2025-01-25 17:43:34 +0800

    feat: dim_agents_agencies_scd

---
+++ estate_agents_dbt/models/dimensions/dim_agents_agencies_scd.sql
@@ -0,0 +1,31 @@
+with
+    stg_agents as (
+        select
+            agent_registration_number,
+            agent_registration_validity_from,
+            agent_registration_validity_to,
+            agency_name,
+            agency_license_number,
+            last_updated_at_utc,
+        from {{ ref("stg_estate_agents__agents") }}
+    ),
+
+    cleaning as (
+        select
+            agent_registration_number,
+            agent_registration_validity_from,
+            coalesce(
+                lead(agent_registration_validity_from) over (
+                    partition by agent_registration_number order by last_updated_at_utc
+                )
+                - 1,
+                agent_registration_validity_to
+            ) as agent_registration_validity_to,
+            agency_name,
+            agency_license_number,
+            last_updated_at_utc,
+        from stg_agents
+    )
+
+select *
+from cleaning
