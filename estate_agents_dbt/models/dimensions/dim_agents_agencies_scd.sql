cf8165b5ceb89873ac69d46d071b6520cdabfffa
jeremychia <jeremy.chia@vinted.com>
2025-01-26 00:35:14 +0800

    feat: scd2 for agency information

--- estate_agents_dbt/models/dimensions/dim_agents_agencies_scd.sql
+++ estate_agents_dbt/models/dimensions/dim_agents_agencies_scd.sql
@@ -13,14 +13,14 @@ with
     cleaning as (
         select
             agent_registration_number,
-            agent_registration_validity_from,
+            agent_registration_validity_from as agent_licence_validity_from,
             coalesce(
                 lead(agent_registration_validity_from) over (
                     partition by agent_registration_number order by last_updated_at_utc
                 )
                 - 1,
                 agent_registration_validity_to
-            ) as agent_registration_validity_to,
+            ) as agent_licence_validity_to,
             agency_name,
             agency_license_number,
             last_updated_at_utc,
@@ -29,3 +29,4 @@ with
 
 select *
 from cleaning
+order by agent_registration_number, agent_licence_validity_from
